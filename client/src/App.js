e144b07657e676e9be1b332bbc92721886333e53
Bruna Stefany da Silva Reinaldo <202015020026@ad.ifpb.edu.br>
2022-05-09 21:09:54 -0300

    FrontEnd Update

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
        
-      <PersonRegister/>
+      <CatRegister/>
     </div>
   );
 }
--- client/src/components/CatRegister.js
+++ client/src/components/CatRegister.js
@@ -5,19 +5,18 @@ export default class CatRegister extends React.Component{
     state = {
         name: '',
         age:0,
-        pelagem:''
+        pelagem:'DEFAULT'
     }
 
     save =() =>{
+        if (this.state.pelagem == "DEFAULT") {
+            alert("Nescessário selecionar pelagem");
+            return;
+        }
         const data = {
             name: this.state.name,
             age: Number.parseInt(this.state.age),
-            pelagem: ()=> {
-                if(this.state.pelagem=="DEFAULT"){
-                    
-                }
-
-            }
+            pelagem: this.state.pelagem
         }
         const rqstOpt = {
             method:'POST',
@@ -31,22 +30,27 @@ export default class CatRegister extends React.Component{
             ).catch(erro=>console.log(erro), alert())
         
     }
+    handleChange(event) {
+        
+        this.setState({pelagem:event.target.value});
 
+    }
     render() {
         return (
             
             <div className="RegisterConteiner">
                 <h1>Adicionar Gato</h1>
                 <form>
-                    <input type="text" placeholder='nome' id='name'/>
-                    <input type="text" placeholder='Idade' id='age'/>
-                    <select>
+                    <input type="text" placeholder='nome' id='name' onChange={(e)=>{this.setState({name: e.target.value})}}/>
+                    <input type="text" placeholder='Idade' id='age'onChange={(e)=>{this.setState({age: e.target.value})}}/>
+
+                    <select onChange={(event)=>this.handleChange(event)}>
                         <option value="DEFAULT">Pelagem</option>
                         <option value="Frajola">Frajola</option>
                         <option value="Escaminha">Escaminha</option>
                         <option value="Tigrado">Tigrado</option>
                     </select>
-                    <button>Cadastrar Gato</button>
+                    <button onClick={this.save}>Cadastrar Gato</button>
                 </form>
             </div>
         );
